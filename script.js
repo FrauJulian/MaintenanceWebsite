ecdea2cf62a55946e761dacd62970956046f4a5c
Julian Lechner <fraujulian@lechner.top>
2026-05-14 22:30:02 +0200

    feat: more languages

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const messages = {
   en: "Sorry we're down for maintenance.",
-  de: "Entschuldigung, wir sind wegen Wartungsarbeiten vor\u00fcbergehend nicht erreichbar.",
+  de: "Entschuldigung, wir sind wegen Wartungsarbeiten offline.",
+  sk: "Prep\u00e1\u010dte, prebieha \u00fadr\u017eba.",
+  cs: "Omlouv\u00e1me se, prob\u00edh\u00e1 \u00fadr\u017eba.",
+  nl: "Sorry, we zijn offline voor onderhoud.",
+  sv: "Tyv\u00e4rr, vi \u00e4r nere f\u00f6r underh\u00e5ll.",
+  no: "Beklager, vi er nede for vedlikehold.",
+  pl: "Przepraszamy, trwa konserwacja.",
+  it: "Spiacenti, siamo offline per manutenzione.",
+  fr: "D\u00e9sol\u00e9s, nous sommes en maintenance.",
   es: "Lo sentimos, estamos en mantenimiento.",
-  fr: "Desoles, nous sommes en maintenance.",
-  it: "Spiacenti, siamo temporaneamente in manutenzione.",
-  nl: "Sorry, we zijn tijdelijk offline wegens onderhoud.",
-  pl: "Przepraszamy, trwaja prace konserwacyjne.",
-  pt: "Desculpe, estamos em manutencao.",
+  sl: "Oprostite, poteka vzdr\u017eevanje.",
+  da: "Beklager, vi er nede for vedligeholdelse.",
 };
 
 const preferredLanguage = (navigator.languages && navigator.languages[0]) || navigator.language || "en";
